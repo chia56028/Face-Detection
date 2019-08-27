331630c05b97f5a57351f9a895f2d86652016a1a
chia56028 <chia56028@gmail.com>
2019-08-27 18:10:15 +0800

    add the function of saving photo

--- MTCNN/faces_mtcnn.py
+++ MTCNN/faces_mtcnn.py
@@ -1,8 +1,22 @@
 import cv2
+import os
+import time
 from mtcnn.mtcnn import MTCNN
 detector = MTCNN()
 
+img_path = "photo"
+if not os.path.exists(img_path):
+    os.mkdir(img_path)
+
+_CAMERA_WIDTH = 640  #攝影機擷取影像寬度
+_CAMERA_HEIGH = 480  #攝影機擷取影像高度
+
 cap = cv2.VideoCapture(0)
+
+# 設定擷取影像的尺寸大小
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, _CAMERA_WIDTH)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, _CAMERA_HEIGH)
+
 while True: 
     #Capture frame-by-frame
     __, frame = cap.read()
@@ -13,22 +27,38 @@ while True:
         for person in result:
             bounding_box = person['box']
             keypoints = person['keypoints']
-    
+
+            x = bounding_box[0]
+            y = bounding_box[1]
+            w = bounding_box[2]
+            h = bounding_box[3]
+
+            # 每隔 n 秒存一次照片
+            n = 5
+            t = int(time.time())
+            padding = 20
+            if t%n==0:
+                face_image = frame[y-padding:y+h+padding, x-padding:x+w+padding]
+                cv2.imwrite(img_path+"/img_item_"+str(t)+".png", face_image)
+            
+            # 框出人臉
             cv2.rectangle(frame,
-                          (bounding_box[0], bounding_box[1]),
-                          (bounding_box[0]+bounding_box[2], bounding_box[1] + bounding_box[3]),
+                          (x, y),
+                          (x+w, y+h),
                           (0,155,255),
                           2)
-    
+
             cv2.circle(frame,(keypoints['left_eye']), 2, (0,155,255), 2)
             cv2.circle(frame,(keypoints['right_eye']), 2, (0,155,255), 2)
             cv2.circle(frame,(keypoints['nose']), 2, (0,155,255), 2)
             cv2.circle(frame,(keypoints['mouth_left']), 2, (0,155,255), 2)
             cv2.circle(frame,(keypoints['mouth_right']), 2, (0,155,255), 2)
+    
     #display resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) &0xFF == ord('q'):
         break
+
 #When everything's done, release capture
 cap.release()
 cv2.destroyAllWindows()
